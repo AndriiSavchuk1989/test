659c672c7ccfd889076079f2ced4c6018d6d315d
Andrij <andrey.m.savchuk@gmail.com>
2019-07-11 17:23:53 +0300

    1111

--- src/InputElement.js
+++ src/InputElement.js
@@ -3,24 +3,49 @@ import React from "react";
 export class InputElement extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { inputValue: 0 };
+    this.state = { inputValue: 0, initialValue: 0 };
+  }
+
+  componentDidMount() {
+    this.setState({
+      inputValue: this.props.value || 1,
+      initialValue: this.props.value || 1
+    });
   }
   onChangeHandler = event => {
     const {
       target: { value }
     } = event;
-    this.changeStateValue(value);
+    if (value.length) {
+      this.changeStateValue(value);
+    }
   };
 
   changeStateValue = value => {
     this.setState({ inputValue: value });
   };
+
+  returnDefaultValue = value => {
+    this.setState({ inputValue: value });
+  };
+
+  onMouseOutHandler = event => {
+    const {
+      target: { value }
+    } = event;
+    console.log("value after mouse out event", value);
+    if (value.length) {
+      this.returnDefaultValue(this.state.initialValue);
+    }
+  };
+
   render() {
     return (
       <input
         type="text"
         value={this.state.inputValue}
         onChange={this.onChangeHandler}
+        onMouseOut={this.onMouseOutHandler}
       />
     );
   }
--- src/index.js
+++ src/index.js
@@ -6,14 +6,5 @@ import { InputElement } from "./InputElement";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
-
 const rootElement = document.getElementById("root");
 ReactDOM.render(<InputElement />, rootElement);
